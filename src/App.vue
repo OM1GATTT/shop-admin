eb1093658970ba5f83d465d9cfcae58be0164a01
om1ga <845085139@qq.com>
2023-04-10 20:17:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+  <button class="btn m-4">按钮</button>
+  <RouterView></RouterView>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.btn {
+  @apply bg-red-500 text-white px-10 py-2 rounded-full hover: (bg-red-700) focus:(ring-8 ring-red-400) transition-all duration-500
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    WindiCSS(),
+  ],
+  // 配置src的别名
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src")
+    }
+  }
+})
+
+
