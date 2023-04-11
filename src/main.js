e1aea551db441e52c28a0b55383c9314748d9b59
om1ga <845085139@qq.com>
2023-04-11 14:23:43 +0800

    登录页表单校验

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import { createApp } from 'vue'
+import App from './App.vue'
+
+// ElementPlus相关
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue' //图标使用->参考官方文档
+
 import 'virtual:windi.css'
+
 import router from './router'
 
 
 const app = createApp(App)
+app.use(router)
 
 app.use(ElementPlus)
-app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 
 app.mount('#app')
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,6 @@
 <template>
     <el-row class="w-full h-screen">
-        <el-col :span="16" class="bg-indigo-500 flex flex-col text-white justify-center items-center">
+        <el-col :span="16" :md="12" class="bg-indigo-500 flex flex-col text-white justify-center items-center">
             <p class="text-light-50 text-5xl font-bold">shop admin</p>
             <p class="text-3xl">shop admin</p>
         </el-col>
@@ -12,19 +12,31 @@
                 <span>账号密码登录</span>
                 <span class="h-[1px] w-16 bg-gray-400"></span>
             </div>
-            <el-form>
 
-                <el-form-item label="Username" placeholder="Username">
-                    <el-input />
+            <el-form :model="form" :rules="rules" ref="formRef">
+                <el-form-item label="Username" prop="username">
+                    <el-input placeholder="Username" v-model="form.username">
+                        <template #prefix>
+                            <el-icon>
+                                <user />
+                            </el-icon>
+                        </template>
+                    </el-input>
                 </el-form-item>
 
-                <el-form-item label="Password">
-                    <el-input type="password" placeholder="Password" />
+                <el-form-item label="Password" prop="password">
+                    <el-input type="password" placeholder="Password" v-model="form.password">
+                        <template #prefix>
+                            <el-icon>
+                                <Lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
                 </el-form-item>
 
 
                 <el-form-item>
-                    <el-button class="w-full py-4 bg-indigo-600 text-white rounded-full" type="primary"
+                    <el-button @click="onSubmit" class="w-full py-4 bg-indigo-600 text-white rounded-full" type="primary"
                         size="default">登录</el-button>
                 </el-form-item>
             </el-form>
@@ -33,7 +45,41 @@
 </template>
 
 <script setup>
+import { reactive, ref } from 'vue';
 
+const form = reactive({
+    username: '',
+    password: ''
+})
+
+const rules = {
+    username: [
+        {
+            required: true,
+            message: '用户名不能为空',
+            trigger: 'blur'
+        }
+    ],
+    password: [
+        {
+            required: true,
+            message: '密码不能为空',
+            trigger: 'blur'
+        }
+    ]
+}
+
+const formRef = ref(null)
+
+const onSubmit = () => {
+    formRef.value.validate((valid) => {
+        if (!valid) {
+            return false;
+        }
+        // 发请求
+        console.log('验证通过')
+    })
+}
 </script>
 
 <style scoped></style>
